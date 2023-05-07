67d610f1e8c80acfc7099354b2753b04f136740e
anshu <anssharma@roku.com>
2023-05-07 17:20:27 +0530

    init

---
+++ App.js
@@ -0,0 +1,27 @@
+ 
+ // 1. create element/tag put sth in it (data) using react
+    // 2. create root from ReactDOM
+    // 3. render element inside root
+    const heading = React.createElement("h1", {id: "heading"}, "HEllow world from react ") 
+    //takes 3 args tag, obj, content of tag
+    //obj contains attributes to element
+ 
+    //----------------------------------------
+
+    //nested structure
+
+    //v.imp 
+    // ReactElement(object) => HTML(Browser understands)
+    //parent here is object
+    const parent = React.createElement('div',{id: "parent"},
+    React.createElement('div',{id: "child"},
+  [  
+    React.createElement('h1',{},"Hello I'm H1 tag inside child"),
+    React.createElement('h2',{},"Hello I'm H2 tag inside child"),
+
+])
+    )
+
+    const root  = ReactDOM.createRoot(document.getElementById('root'));
+
+    root.render(parent);
